453a3723b62fc7b1a62e1c7c437bb0629a1ed94d
Joshua Sharp <joshuafsharp@gmail.com>
2022-10-01 23:27:26 +1000

    wip: add tsx support

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,10 @@
-<template>
-  <div class="container" style="padding: 50px 0 100px 0">
-    <Profile v-if="store.user" />
-    <Auth v-else />
-  </div>
-</template>
-
-<script>
+import {defineComponent} from 'vue'
   import { store } from './store'
   import { supabase } from './supabase'
   import Auth from './components/Auth.vue'
   import Profile from './components/Profile.vue'
 
-  export default {
-    components: {
-      Auth,
-      Profile,
-    },
-
+  export default defineComponent({
     setup() {
       store.user = supabase.auth.user()
       supabase.auth.onAuthStateChange((_, session) => {
@@ -27,5 +15,15 @@
         store,
       }
     },
-  }
-</script>
+
+
+render() {
+return (
+
+  <div class="container" style="padding: 50px 0 100px 0">
+    <Profile v-if="store.user" />
+    <Auth v-else />
+  </div>
+    )
+}
+  })
