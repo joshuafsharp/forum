0c2ccbca53e21a70dac74df0baa6a315c5e348f9
Joshua Sharp <joshuafsharp@gmail.com>
2022-10-02 20:28:48 +1100

    wip: remove junk

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,11 @@ import * as path from 'path';
 import vue from '@vitejs/plugin-vue';
 import vueJsx from '@vitejs/plugin-vue-jsx';
 import { defineConfig } from 'vite';
-import eslintPlugin from 'vite-plugin-eslint';
 import ssr from 'vite-plugin-ssr/plugin';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueJsx(), ssr(), eslintPlugin()],
+  plugins: [vue(), vueJsx(), ssr()],
   resolve: {
     alias: {
       '~': path.resolve(__dirname, '/src'),
